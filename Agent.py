08500af9d82bb42fad6cbdf560c6871654ce0f8c
JayTheYggdrasil <thatotherrandomguy@gmail.com>
2017-09-20 23:03:22 -0400

    Fixed reward attribution
    Loss function now correctly includes the reward instead of improperly doing sillyness

--- Agent.py
+++ Agent.py
@@ -4,20 +4,18 @@ class RL:
         self._x=tf.placeholder(tf.float32, shape = inShape)
         self.L=[tf.reshape( self._x, [-1, 7, 6, 1]) ]
         
-        self.L.append( self.conv2d( self.L[-1], self.W([4, 4, 1, 10]) ) )
-        self.L.append( tf.reshape(self.L[-1], [1,inShape[1]*10]))
-        self.L.append( self.dense(self.L[-1], [inShape[1]*10,7]) ) #Output size
+        self.L.append( self.conv2d( self.L[-1], self.W([4, 4, 1, 20]) ) )
+        self.L.append( tf.reshape(self.L[-1], [1,inShape[1]*20]))
+        self.L.append( self.dense(self.L[-1], [inShape[1]*20,7]) ) #Output size
         
-        self.targ = tf.placeholder(tf.float32)
-        self.loss = tf.reduce_mean(
-            tf.losses.softmax_cross_entropy(self.targ, self.L[-1]))
+        self.reward = tf.placeholder(tf.float32)
+        self.loss = tf.reduce_mean(self.reward * tf.log(self.L[-1]))
 
         self.optimizer = tf.train.RMSPropOptimizer(0.02)
         self.train_step=self.optimizer.minimize(self.loss)
 
-    def Update(self, state, action, reward, lr=0.01):
-        self.optimizer.learning_rate=lr*reward
-        self.train_step.run( feed_dict = { self._x: state, self.targ: action})
+    def Update(self, state, reward):
+        self.train_step.run( feed_dict = { self._x: state, self.reward: reward})
 
     def predict(self, state):
         return self.L[-1].eval(feed_dict = {self._x: [state]} )
--- Training.py
+++ Training.py
@@ -5,7 +5,7 @@ import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser(description='Connect 4 reinforcement learning trainer')
-parser.add_argument('--maxgames', type=int, default='100000',
+parser.add_argument('--maxgames', type=int, default='10000',
                     help='maximum number of games to play')
 parser.add_argument('--loginterval', type=int, default='100',
                     help='how often to print the game number')
@@ -47,43 +47,40 @@ def test(player): #Player vs AI: param: what player you want to be, 1 or 2, anyt
 
 sess=tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
-Games=100000
-discount=0.35
-G=0
-with tf.device('/cpu:0'):
-    while True:
-        G+=1
-=======
+Games=1000000
 discount=0.5
-for game in range(args.maxgames):
-        if game % args.loginterval == 0: 
-            print("Game {:9d} out of {:9d}.".format(game, args.maxgames))
-        Obs=Env.reset()
-        R1=[]
-        R2=[]
-        Actions1=[]
-        Actions2=[]
-        while True:
-            Actions1.append([Agent1.predict(Obs), Obs])
-            Obs, R, done = Env.step(np.argmax(Actions1[-1][0]))
-            R1.append(R)
-            if done:
-                R2.append(-R)
-                break
-            Actions2.append([Agent2.predict(Obs), Obs])
-            Obs, R, done = Env.step(np.argmax(Actions2[-1][0]))
-            R2.append(R)
-            if done:
-                R1.append(-R)
-                break
-        for a in range(1,len(Actions1)+1):
-            targ = [0] * 7
-            targ[np.argmax(Actions1[-a][0])] = 1
-            Agent1.Update([Actions1[-a][1]], targ, sum(R1)*discount**a)
-                
+G=0
+with tf.device('/gpu:0'):
+        discount=0.5
+        for game in range(args.maxgames):
+                if game % args.loginterval == 0: 
+                    print("Game {:9d} out of {:9d}.".format(game, args.maxgames))
+                Obs=Env.reset()
+                R1=[]
+                R2=[]
+                Actions1=[]
+                Actions2=[]
+                while True:
+                    Actions1.append([Agent1.predict(Obs), Obs])
+                    Obs, R, done = Env.step(np.argmax(Actions1[-1][0]))
+                    R1.append(R)
+                    if done:
+                        R2.append(-R)
+                        break
+                    Actions2.append([Agent2.predict(Obs), Obs])
+                    Obs, R, done = Env.step(np.argmax(Actions2[-1][0]))
+                    R2.append(R)
+                    if done:
+                        R1.append(-R)
+                        break
+                for a in range(1,len(Actions1)+1):
+                    targ = [0] * 7
+                    targ[np.argmax(Actions1[-a][0])] = 1
+                    Agent1.Update([Actions1[-a][1]], sum(R1)*discount**a)
+                        
 
-        for a in range(1,len(Actions2)+1):
-            targ = [0] * 7
-            targ[np.argmax(Actions2[-a][0])] = 1
-            Agent1.Update([Actions2[-a][1]], targ, sum(R2)*discount**a)
+                for a in range(1,len(Actions2)+1):
+                    targ = [0] * 7
+                    targ[np.argmax(Actions2[-a][0])] = 1
+                    Agent1.Update([Actions2[-a][1]], sum(R2)*discount**a)
     
